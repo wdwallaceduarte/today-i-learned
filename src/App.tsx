c7e4050a0e8448455d291109f06ee23b7814c342
Wallace Duarte <wd.wallaceduarte@gmail.com>
2026-05-07 21:55:56 -0300

    aula sobre renderização de listas

--- src/App.tsx
+++ src/App.tsx
@@ -1,122 +1,122 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+/**  -- PROPS --
+  function Button(props) {
+  return <button>{props.text}</button>
+}
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  return (
+    <>
+      <Button text='Click' />
+      <Button text='Send' />
+      <Button text='Cancel' />
+    </>
+  )
+}
+ */
+
+/**  1ESTADO 
+import { useState } from "react"
+
+export default function App() {
+  let contador = 0
+
+
+  const [copiaContador, setCopiaContador] = useState(contador)
+
+  function count() {
+    setCopiaContador(copiaContador + 1)
+    console.log(contador)
+  }
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+      <span>{copiaContador}</span>
+      <button onClick={count}>+1</button>
+    </>
+  )
 
-      <div className="ticks"></div>
+}
+*/
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+/** RENDERIZAÇÃO
+export default function App() {
+  const aluno = {
+    id: 1,
+    nome: 'Walace Duarte',
+    curso: 'Desenvolvimento Web Full Fullstack',
+    estado: 'ativo'
+  }
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+  return (
+    <>
+      {aluno.estado && (
+        `O aluno ${aluno.nome} está ${aluno.estado}.`
+      )}
+    </>
+  )
+}
+*/
+
+/** RENDERIZAÇÃO CONDICIONAL 
+export default function App() {
+  const aluno = {
+    id: 1,
+    nome: 'Walace Duarte',
+    curso: 'Desenvolvimento Web Full Fullstack',
+    estado: true
+  }
+
+  return (
+    <>
+      {aluno.estado ? 'Aluno ativo' : 'Aluno passivo'}
+    </>
+  )
+}
+*/
+
+/** RENDERIZAÇÃO DE LISTASA */
+
+function NewFact(props) {
+  return (
+    <>
+     <p>{props.text}</p>
+     <a href={props.source}>Fonte</a>
+    </>
+  )
+}
+import type { Fact } from "./types"
+
+export default function App() {
+  const facts: Fact[] = [
+    {
+      id: 2,
+      text: 'Digital Collage é nomeada a melhor escola de tecnologia de Fortaleza.',
+      source: 'https://opovo.com',
+      category: 'Tecnologia',
+      votes_interesting: 2,
+      votes_mindblowing: 0,
+      votes_false: 5,
+      created_at: '2026-05-07 19:35:03 GMT-3'
+    },
+    {
+      id: 1,
+      text: 'Neymar agride crança negra e pobre de comunidade filho de detento mundialmente conhecido.',
+      source: 'https://g1.com',
+      category: 'Esporte',
+      votes_interesting: 5,
+      votes_mindblowing: 13,
+      votes_false: 2,
+      created_at: '2026-05-05 19:35:03 GMT-3'
+    }
+  ]
+
+  return (
+    <>
+      {facts.map((fact) => {
+        return <NewFact key={fact.id} text={fact.text} source={fact.source} />
+      })}
     </>
   )
 }
 
-export default App
+
--- src/types.ts
+++ src/types.ts
@@ -1,16 +1,17 @@
 export interface Fact {
-    id: number
-    text: string
-    source: string
-    category: string
-    votes_interesting: number
-    votes_mindblowing: number
-    votes_false: number
-    create_at: string
-}
+  id: number;
+  text: string;
+  source: string;
+  category: string;
+  votes_interesting: number;
+  votes_mindblowing: number;
+  votes_false: number;
+  created_at: string;
+};
 
 export interface Category {
- value: string
- label: string
- color: string
-}
+  value: string;
+  label: string;
+  color: string;
+};
+
