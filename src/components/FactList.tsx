b8a43421580c5d39ffd53510b25b71037b4fe3b6
Wallace Duarte <wd.wallaceduarte@gmail.com>
2026-05-14 21:34:30 -0300

    fix: adicionar elemto de lista

--- src/components/FactList.tsx
+++ src/components/FactList.tsx
@@ -2,18 +2,20 @@ import type { Fact } from "../types"
 import FactItem from "./FactItem"
 
 interface FactListProps {
-    facts : Fact[]
+    facts: Fact[]
 }
 
 export default function FactList({ facts }: FactListProps) {
-    if(facts.length === 0) {
-        return <p>Nenhum fato para esta categoria ainda. Crie o primeiro! ✌️</p>
+    if (facts.length === 0) {
+        return <p>Nenhum fato para esta categoria ainda. Crie o primeiro! 🫵 </p>
     }
     return (
         <>
-            {facts.map(fact => {
-                return <FactItem key={fact.id} fact={fact}/>
-            })}
+            <ul>
+                {facts.map(fact => {
+                    return <FactItem key={fact.id} fact={fact} />
+                })}
+            </ul>
         </>
     )
 }
